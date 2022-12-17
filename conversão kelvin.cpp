4d295c97a0a5d17bdeb38c06aa0fbf957032d6b0
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2022-12-16 23:11:59 -0300

    Update conversão kelvin.cpp

--- conversão kelvin.cpp
+++ conversão kelvin.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-float celsiusParaFahrenheite(float c);
-float fahrenheiteParaCelsius(float f);
+float celsiusParaFahrenheit(float c);
+float fahrenheitParaCelsius(float f);
 float celsiusParaKelvin(float k);
 
 int main(){
@@ -11,33 +11,37 @@ int main(){
 	
 	printf("Conversor de temperatura\n");
 	printf("Escolha uma opcao: \n");
-	printf("1 - Celsius para Fahrenheite\n");
-	printf("2 - Fahrenheite para Celsius\n");
+	printf("1 - Celsius para Fahrenheit\n");
+	printf("2 - Fahrenheit para Celsius\n");
 	printf("3 - Celsius para kelvin\n");
 	scanf("%d", &op);
+	
 	printf("Digite o valor da temperatura ");
 	if(op == 1){
 		printf(" em Celsius: ");
 		scanf("%f", &tc);
-		tf = celsiusParaFahrenheite(tc);
+		tf = celsiusParaFahrenheit(tc);
 	}else if(op == 2){
-		printf(" em Fahrenheite: ");
+		printf(" em Fahrenheit: ");
 		scanf("%f", &tf);
+		tc = fahrenheitParaCelsius(tf);
 	}else {
-		printf(" em Kelvin: ");
-		scanf("%f", &tk);
+		printf(" em Celsius: ");
+		scanf("%f", &tc);
 		tk = celsiusParaKelvin(tc);
 	}
-	printf("Temperatura em Celsius: %.2f C\n", tc);
-	printf("Temperatura em Fahrenheite: %.2f C\n", tf);
-	printf("Temperatura em Lelvin: %.2f C\n", tk);
+	printf("Temperatura em Celsius: %.2f ºC \n", tc);
+	printf("Temperatura em Fahrenheit: %.2f ºF \n", tf);
+	printf("Temperatura em Kelvin: %.2f ºK \n", tk);
+	
+	return 0;
 }
-float celsiusParaFahrenheite(float c){
-	return  c*1.8+32;
+float celsiusParaFahrenheit(float c){
+	return  c * 1.8 + 32;
 }
-float fahrenheiteParaCelsius(float f){
+float fahrenheitParaCelsius(float f){
 	return (f - 32)/1.8;
 }
-float celsiusParaKelvin(float k){
-	return k + 273.15;
+float celsiusParaKelvin(float c){
+	return c + 273.15;
 }
